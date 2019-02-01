f56d40625b211644ee70733872d1446803df3f46
Toews <wylie.toews@uleth.ca>
2019-01-31 17:15:09 -0700

    added panels anad other things

---
+++ www/js/scripts.js
@@ -0,0 +1,13 @@
+var app = new Framework7 ({
+    
+    root: "#app", /* THIS IS THE APP ELEMENT */
+    routes: [
+        {
+            path:'/page2/',
+            url: 'pages/page2.html',
+        }
+    ]
+    
+})
+
+var mainView = app.views.create('.view-main');
